d00a2caee1df9a59d611e1f90e043cd721ee30a8
Julian Thijssen <julianthijssen@gmail.com>
2026-01-03 01:25:28 +0100

    Add extra file paths

--- src/PatchSeqFilePaths.h
+++ src/PatchSeqFilePaths.h
@@ -11,10 +11,30 @@ public:
 
     bool allFilesLocated();
 
+    bool hasGeneExpressions() { return !gexprFilePath.isNull() && !gexprFilePath.isEmpty(); }
+    bool hasEphysFeatures() { return !ephysFilePath.isNull() && !ephysFilePath.isEmpty(); }
+    bool hasMorphologyFeatures() { return !morphoFilePath.isNull() && !morphoFilePath.isEmpty(); }
+
+    bool hasEphysUMap() { return !ephysUMapFilePath.isNull() && !ephysUMapFilePath.isEmpty(); }
+    bool hasMorphoUMap() { return !morphoUMapFilePath.isNull() && !morphoUMapFilePath.isEmpty(); }
+    bool hasTxUMap() { return !txUMapFilePath.isNull() && !txUMapFilePath.isEmpty(); }
+    bool hasMEUMap() { return !meUMapFilePath.isNull() && !meUMapFilePath.isEmpty(); }
+
+    bool hasEphysTraces() { return !ephysTracesDir.isNull() && !ephysTracesDir.isEmpty(); }
+    bool hasMorphologies() { return !morphologiesDir.isNull() && !morphologiesDir.isEmpty(); }
+
 public:
     QString gexprFilePath;
     QString ephysFilePath;
     QString morphoFilePath;
     QString metadataFilePath;
     QString annotationFilePath;
+
+    QString ephysUMapFilePath;
+    QString morphoUMapFilePath;
+    QString txUMapFilePath;
+    QString meUMapFilePath;
+
+    QString ephysTracesDir;
+    QString morphologiesDir;
 };
